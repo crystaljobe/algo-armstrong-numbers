667280f23f7cac16d322a696c18328e2b6097db9
crystaljobe <crystaljobe29@gmail.com>
2024-02-03 23:15:22 -0700

    created functions in javascript and python for armstrong nums

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,20 @@
-# How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    armstrongNums = []
+    for num in numbers: 
+        if num <= 9 :
+            armstrongNums.append(num)
+        else :
+            numStr = str(num)
+            exponent = len(numStr)
+            powerSum = 0
+            for char in numStr:
+                powerSum += int(char) ** exponent
+            if powerSum == num :
+                armstrongNums.append(num)
+    return armstrongNums
+            
+            
+            
+            
+
+#find_armstrong_numbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 372, 407])
